c2d8b462d675983a69a019827288ac0d8c905136
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-05-21 04:01:16 -0500

    feat: :alembic: tables checking

--- js/modulesComponents/tables.js
+++ js/modulesComponents/tables.js
@@ -11,7 +11,7 @@ export const tableRocketColum1 = async (Rocket)=>{
 
     let div = document.createElement("div");
     div.classList.add("table__container__1");
-    // 
+    
     let div1 = document.createElement("div");
     let span1 = document.createElement("span");
     span1.textContent = "Rocket in service"
@@ -19,15 +19,12 @@ export const tableRocketColum1 = async (Rocket)=>{
     strong1.textContent = `${(Rocket.active) ? "Active" : "Low" }`
     div1.append(span1, strong1)
 
-    // 
     let div2 = document.createElement("div");
     let span2 = document.createElement("span");
     span2.textContent = "Number of stages"
     let strong2 = document.createElement("strong");
     strong2.textContent = `${Rocket.stages}`
     div2.append(span2, strong2)
-    
-    // 
     let div3 = document.createElement("div");
     let span3 = document.createElement("span");
     span3.textContent = "Number of propellants"
@@ -35,7 +32,6 @@ export const tableRocketColum1 = async (Rocket)=>{
     strong3.textContent = `${Rocket.boosters}`
     div3.append(span3, strong3)
 
-    // 
     let div4 = document.createElement("div");
     let span4 = document.createElement("span");
     span4.textContent = "Type"
@@ -43,7 +39,6 @@ export const tableRocketColum1 = async (Rocket)=>{
     strong4.textContent = `${Rocket.type}`
     div4.append(span4, strong4)
 
-    // 
     let div5 = document.createElement("div");
     let span5 = document.createElement("span");
     span5.textContent = "Landing legs"
@@ -51,7 +46,6 @@ export const tableRocketColum1 = async (Rocket)=>{
     strong5.textContent = `${Rocket.landing_legs.number}`
     div5.append(span5, strong5)
 
-    // 
     let div6 = document.createElement("div");
     let span6 = document.createElement("span");
     span6.textContent = "Leg material"
@@ -61,17 +55,6 @@ export const tableRocketColum1 = async (Rocket)=>{
 
     div.append(div4, div1, div2, div3, div5, div6)
     information__table__1.append(div)
-    // 
-
-    // <h3>Title</h3>
-    // <hr>
-    // <div class="table__container__1">
-    //     <div>
-    //         <span>Title</span>
-    //         <strong>Hola</strong>
-    //     </div>
-        
-    // </div>
 }
 export const tableRocketColum2 = async (Rocket)=>{
     let information__table__2 = document.querySelector("#information__table__2");
@@ -83,7 +66,7 @@ export const tableRocketColum2 = async (Rocket)=>{
 
     let div = document.createElement("div");
     div.classList.add("table__container__1");
-    // 
+    
     let div1 = document.createElement("div");
     let span1 = document.createElement("span");
     span1.textContent = "Maximum power loss"
@@ -91,7 +74,6 @@ export const tableRocketColum2 = async (Rocket)=>{
     strong1.textContent = `${(Rocket.engines.engine_loss_max) ? Rocket.engines.engine_loss_max : 0}`
     div1.append(span1, strong1)
 
-    // 
     let div2 = document.createElement("div");
     let span2 = document.createElement("span");
     span2.textContent = "Engine availability"
@@ -99,7 +81,6 @@ export const tableRocketColum2 = async (Rocket)=>{
     strong2.textContent = `${(Rocket.engines.layout) ? Rocket.engines.layout : ""}`
     div2.append(span2, strong2)
 
-    // 
     let div3 = document.createElement("div");
     let span3 = document.createElement("span");
     span3.textContent = "Number of engines"
@@ -107,7 +88,6 @@ export const tableRocketColum2 = async (Rocket)=>{
     strong3.textContent = `${(Rocket.engines.number) ? Rocket.engines.number : 0}`
     div3.append(span3, strong3)
 
-    // 
     let div4 = document.createElement("div");
     let span4 = document.createElement("span");
     span4.textContent = "Stage 1 fuel"
@@ -115,7 +95,6 @@ export const tableRocketColum2 = async (Rocket)=>{
     strong4.textContent = `${(Rocket.engines.propellant_1) ? Rocket.engines.propellant_1 : ""}`
     div4.append(span4, strong4)
 
-    // 
     let div5 = document.createElement("div");
     let span5 = document.createElement("span");
     span5.textContent = "Stage 2 fuel"
@@ -123,7 +102,6 @@ export const tableRocketColum2 = async (Rocket)=>{
     strong5.textContent = `${(Rocket.engines.propellant_2) ? Rocket.engines.propellant_2 : ""}`
     div5.append(span5, strong5)
 
-    // 
     let div6 = document.createElement("div");
     let span6 = document.createElement("span");
     span6.textContent = "Type"
